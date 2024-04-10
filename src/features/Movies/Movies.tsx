b7ca5edacbd84f8c9f1f5c3ccce191f006488831
Annazubakha <korepovaanna@gmail.com>
2024-04-10 12:11:00 +0200

    custom hook

--- src/features/Movies/Movies.tsx
+++ src/features/Movies/Movies.tsx
@@ -1,4 +1,4 @@
-import { Movie, fetchMovies } from "../../reducers/movies";
+import { FEtchNextPage, Movie } from "../../reducers/moviesSlice";
 import { connect } from "react-redux";
 import { RootState } from "../../store";
 import { MovieCard } from "./MovieCard";
@@ -6,45 +6,54 @@ import { useContext, useEffect } from "react";
 import { useAppDispatch } from "../../hooks";
 import { Container, Grid, LinearProgress, Typography } from "@mui/material";
 import { AuthContext, anonymousUser } from "../../AuthContext";
+import { useIntersectionObserver } from "../../hooks/useIntersectionObserver";
 
 interface MoviesProps {
   movies: Movie[];
   loading: boolean;
+  hasMorePages: boolean;
 }
-export function Movies({ movies, loading }: MoviesProps) {
+export function Movies({ movies, loading, hasMorePages }: MoviesProps) {
   const dispatch = useAppDispatch();
+
+  // const movies = useAppSelector((state) => state.movies.top);
+  // const loading = useAppSelector((state) => state.movies.loading);
+  // const hasMorePages = useAppSelector((state) => state.movies.hasMorePages);
 
   const { user } = useContext(AuthContext);
   const loggedIn = user !== anonymousUser;
+  const [targetRef, entry] = useIntersectionObserver();
 
   useEffect(() => {
-    dispatch(fetchMovies());
-  }, [dispatch]);
+    if (entry?.isIntersecting && hasMorePages) {
+      dispatch(FEtchNextPage());
+    }
+  }, [dispatch, entry?.isIntersecting, hasMorePages]);
 
   return (
     <Container sx={{ py: 15 }} maxWidth="lg">
       <Typography variant="h4" align="center" sx={{ mb: 4 }} color="primary">
         Now playing movies
       </Typography>
-      {loading ? (
-        <LinearProgress color="secondary" />
-      ) : (
-        <Grid container spacing={4}>
-          {movies.map((m) => (
-            <Grid item key={m.id} xs={12} sm={6} md={4}>
-              <MovieCard
-                id={m.id}
-                key={m.id}
-                title={m.title}
-                release_date={m.release_date}
-                popularity={m.popularity}
-                image={m.image}
-                enableUserActions={loggedIn}
-              />
-            </Grid>
-          ))}
-        </Grid>
-      )}
+
+      <Grid container spacing={4}>
+        {movies.map((m) => (
+          <Grid item key={m.id} xs={12} sm={6} md={4}>
+            <MovieCard
+              id={m.id}
+              key={m.id}
+              title={m.title}
+              release_date={m.release_date}
+              popularity={m.popularity}
+              image={m.image}
+              enableUserActions={loggedIn}
+            />
+          </Grid>
+        ))}
+      </Grid>
+      <div ref={targetRef}>
+        {loading && <LinearProgress color="secondary" sx={{ mt: 3 }} />}
+      </div>
     </Container>
   );
 }
@@ -52,6 +61,7 @@ export function Movies({ movies, loading }: MoviesProps) {
 const mapStateToProps = (state: RootState) => ({
   movies: state.movies.top,
   loading: state.movies.loading,
+  hasMorePages: state.movies.hasMorePages,
 });
 const connector = connect(mapStateToProps);
 
